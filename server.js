4555ed631911efb02e8065075f5203308dabe714
Eddie Hsiao <eddiehsiao@Eddies-iMac.local>
2020-09-07 21:56:19 -0700

    try NODE_TLS_REJECT_UNAUTHORIZED

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true
+    ssl: true,
   }
 });
 
@@ -24,7 +26,7 @@ app.use(cors());
 
 app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
